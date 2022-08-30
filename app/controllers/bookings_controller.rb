febf134771c0c612201ab26250906abbdfa59a55
Lois Graham <loisgraham12@gmail.com>
2022-08-30 13:04:49 +0100

    Added to routes and added new method to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,6 @@
 class BookingsController < ApplicationController
+  def new
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :bookings, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
