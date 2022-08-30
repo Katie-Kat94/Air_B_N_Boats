795c56a22bdcf8c8035695159491fb7b1c7b1447
Lois Graham <loisgraham12@gmail.com>
2022-08-30 11:48:12 +0100

    Added create methods to boats controller  and simple form for new view

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -6,4 +6,20 @@ class BoatsController < ApplicationController
   def show
     @boat = Boat.find(params[:id])
   end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.save
+    redirect_to_boat_path(boat)
+  end
+
+  private
+
+  def boat_params
+    params.require(:restaurant).permit(:name, :category, :available, :capacity, :location, :price, :rating)
+  end
 end
