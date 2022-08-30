2cde0dcd3378303ee1010710e8465f30f0af186b
Lois Graham <loisgraham12@gmail.com>
2022-08-30 15:19:21 +0100

    Made create method work in bookings controller, added authenticate user before actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,29 @@
 class BookingsController < ApplicationController
+  before_action :set_boat, only: [:new, :create]
+
   def new
     @boat = Boat.find(params[:boat_id])
     @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.boat = @boat
+    @booking.user = current_user
+    if @booking.save
+      redirect_to boats_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_boat
+    @boat = Boat.find(params[:boat_id])
   end
 end
