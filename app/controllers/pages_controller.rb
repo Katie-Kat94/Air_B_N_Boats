f9713b63498b57214d7483cbbdd7a3e61dab2bb1
Blake McKenzie <blakemckenzie365@gmail.com>
2022-09-01 12:20:27 +0100

    searchbar addded to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+
   def home
-    @boats = Boat.all
+    if params[:query].present? && @boats = Boat.search_by_category(params[:query]) != []
+      @boats = Boat.search_by_category(params[:query])
+    else
+      @boats = Boat.all
+    end
+
   end
 end
